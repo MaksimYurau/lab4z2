47b9c69d144e7d21f5669bcf2cd1f162107e389e
MaksimYurau <2004softwaredeveloper@gmail.com>
2023-10-02 00:58:17 +0300

    Updated lab4z2.cpp.

--- lab4z2/lab4z2.cpp
+++ lab4z2/lab4z2.cpp
@@ -1,4 +1,17 @@
-﻿#include <iostream>
+﻿/*
+Лабораторная работа №4.
+Потоки ввода-вывода.
+Задание 2.
+Следующее задание требуется решить с привлечением текстовых
+файлов. Нужно написать функцию, с помощью которой подготовить входной
+файл, записав в него 100 случайных целых чисел в диапазоне от -50 до +50 по
+одному на строке. Сформировать выходной файл, преобразовав числа входного
+файла. 
+9. Записать выходной файл, умножить каждое четное число на первое
+отрицательное число файла.
+*/
+
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <cstdlib>
@@ -9,11 +22,9 @@ int main() {
 
     std::string input_filename, output_filename;
 
-    // Запрос пути к входному файлу
     std::cout << "Введите путь к входному файлу (например, input.txt): ";
     std::cin >> input_filename;
 
-    // Запрос пути к выходному файлу
     std::cout << "Введите путь к выходному файлу (например, output.txt): ";
     std::cin >> output_filename;
 
@@ -21,9 +32,8 @@ int main() {
     std::ofstream input_file(input_filename);
 
     // std::ofstream output_file("E:\output.txt");
-    std::ofstream output_file(output_filename);
+    // std::ofstream output_file(output_filename);
 
-    // Подготовка входного файла
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
     for (int i = 0; i < 100; ++i) {
         int number = std::rand() % 101 - 50;
@@ -32,7 +42,6 @@ int main() {
 
     input_file.close();
 
-    // Чтение входного файла, преобразование и запись выходного файла
     std::ifstream input(input_filename);
     std::vector<int> numbers;
     int first_negative = 0;
@@ -61,7 +70,8 @@ int main() {
 
     std::cout << std::endl;
     std::cout << "Исходный файл. " << output_filename << std::endl;
-    std::cout << "Преобразование завершено. Результат записан в " << output_filename << std::endl;
+    std::cout << "Преобразование завершено. Результат записан в " << output_filename << std::endl << std::endl;
 
+    system("pause");
     return 0;
 }
