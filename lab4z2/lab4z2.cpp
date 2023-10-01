b56e00a338081bd33e57d4a6a12fb5211042cadd
MaksimYurau <2004softwaredeveloper@gmail.com>
2023-10-02 00:36:21 +0300

    Updated lab4z2.cpp.

--- lab4z2/lab4z2.cpp
+++ lab4z2/lab4z2.cpp
@@ -5,8 +5,23 @@
 #include <ctime>
 
 int main() {
-    std::ofstream input_file("E:\input.txt");
-    std::ofstream output_file("E:\output.txt");
+    setlocale(0, "Ru");
+
+    std::string input_filename, output_filename;
+
+    // Запрос пути к входному файлу
+    std::cout << "Введите путь к входному файлу (например, input.txt): ";
+    std::cin >> input_filename;
+
+    // Запрос пути к выходному файлу
+    std::cout << "Введите путь к выходному файлу (например, output.txt): ";
+    std::cin >> output_filename;
+
+    // std::ofstream input_file("E:\input.txt");
+    std::ofstream input_file(input_filename);
+
+    // std::ofstream output_file("E:\output.txt");
+    std::ofstream output_file(output_filename);
 
     // Подготовка входного файла
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
@@ -18,7 +33,7 @@ int main() {
     input_file.close();
 
     // Чтение входного файла, преобразование и запись выходного файла
-    std::ifstream input("E:\input.txt");
+    std::ifstream input(input_filename);
     std::vector<int> numbers;
     int first_negative = 0;
 
@@ -34,7 +49,7 @@ int main() {
 
     input.close();
 
-    std::ofstream output("E:\output.txt");
+    std::ofstream output(output_filename);
 
     for (int number : numbers) {
         if (number % 2 == 0) {
@@ -43,6 +58,10 @@ int main() {
     }
 
     output.close();
+
+    std::cout << std::endl;
+    std::cout << "Исходный файл. " << output_filename << std::endl;
+    std::cout << "Преобразование завершено. Результат записан в " << output_filename << std::endl;
 
     return 0;
 }
